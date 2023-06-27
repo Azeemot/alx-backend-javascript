bb1023d30f38509c11f5e0b5d61b013e2701994b
Azeemot <bolanleazeemah@gmail.com>
2023-06-26 17:57:03 -0700

    test

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;    // Update the value of 'task' variable to 'true'
+    task2 = false;  // Update the value of 'task2' variable to 'false'
   }
 
   return [task, task2];
